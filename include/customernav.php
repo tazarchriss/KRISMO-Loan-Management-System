5eb08c9d315a0ee3c6ec052310c8a8bcf6165994
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-05-14 22:13:40 +0300

    Create customernav.php

---
+++ include/customernav.php
@@ -0,0 +1,64 @@
+<!-- This is  the page for customer's side navbar -->
+
+<div class="fixed-right mt-1" id="navbarNav">
+          <nav class="col-md-12" >
+            <ul class="" style="list-style: none;">
+              <li class="nav-item mb-1"  style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a  class="nav-link text-light"  href="home.php" >
+                  <i class="fa fa-home text-danger"></i> &nbsp; Dashboard
+                  
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1" style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a  class="nav-link text-light"  href="notification.php">
+                  <i class="fas fa-bullhorn  text-danger"></i> &nbsp; Notifications
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1 " style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a  class="nav-link text-light"  href="account.php" >
+                  <i class=" fas fa-credit-card  text-danger"></i>&nbsp; &nbsp; Account Balance
+                </a>
+              
+              </li>
+
+              <li class="nav-item mb-1 " style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a href="loan_request.php" class="nav-link text-light">
+                  <i class="fas fa-clipboard-list  text-danger"></i> &nbsp; &nbsp; Request Loan
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1 " style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a href="loan_request.php" class="nav-link text-light">
+                  <i class="fas fa-clipboard-list  text-danger"></i> &nbsp; &nbsp; Request Status
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1" style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a href="active_loans.php" class="nav-link text-light">
+                  <i class="far fa-money-bill-alt text-danger"></i> &nbsp;&nbsp;  Active Loans
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1" style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a href="loan_history.php" class="nav-link text-light">
+                  <i class="far fa-file text-danger"></i> &nbsp;&nbsp;&nbsp; Loans History
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1" style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a href="profile.php" class="nav-link text-light">
+                  <i class="fas fa-address-book  text-danger"></i> &nbsp; &nbsp; Profile
+                </a>
+              
+              </li>
+              <li class="nav-item mb-1" style="border-bottom: 2px solid red;border-left: 1px solid red;background-color: rgb(2, 96, 112);">
+                <a href="instruction.php" class="nav-link text-light">
+                  <i class="fas fa-graduation-cap  text-danger"></i> &nbsp;  Instructions
+                </a>
+              
+              </li>
+            </ul>
+          </nav>
+        </div>
