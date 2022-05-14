46f0ef3b3f0a00507286822cbe220259b75a3476
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-05-14 22:08:53 +0300

    Create authentication.php

---
+++ config/authentication.php
@@ -0,0 +1,53 @@
+<?php
+ session_start();
+require_once('database.php');
+    if(isset($_POST['login'])){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+        $qry="SELECT * FROM user where email='$email' and password='$password' limit 1 ";
+
+        $login=mysqli_query($conn,$qry);
+
+        if(!$login) 
+        {
+            echo mysqli_error($login);
+        }
+        
+        else{
+        $rows=mysqli_num_rows($login);
+        if($rows==0){
+            header('location:../index.php?request=1');
+           
+        }
+        else{
+            $res=mysqli_fetch_array($login);
+            $id=$res['id'];
+            $fname=$res['fname'];
+            $mname=$res['mname'];
+            $lname=$res['lname'];
+            $role=$res['role'];
+            
+
+            // session creation
+            $_SESSION['id']=$id;
+            $_SESSION['fname']=$fname;
+            $_SESSION['mname']=$mname;
+            $_SESSION['lname']=$lname;
+        
+            if($role=='1')
+            {
+                header('Location:../admin.php');
+            }
+            else
+            {
+                header('Location:../home.php');
+            }
+                
+        
+            
+        }
+           
+        }
+    }
+    ?>
