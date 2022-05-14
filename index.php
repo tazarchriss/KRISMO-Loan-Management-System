e729ca77c07356442380cf07882c263170047e91
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-05-14 21:58:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   </head>
   <style>
     body{
-      background-image: url('assets/img/body.jpg');
+      background-image: url('body.jpg');
     
 
     }
@@ -20,7 +20,7 @@
 
           
         <!-- End of navbar -->
-  <body class="bg-light" style="background-image: url('assets/img/body.jpg');">
+  <body class="bg-light" style="background-image: url('body.jpg');">
 
 
         <!-- start of navbar-->
@@ -31,9 +31,9 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item active">
+              <!-- <li class="nav-item active">
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
+              </li> -->
               <!-- <li class="nav-item">
                 <a class="nav-link" href="#">Features</a>
               </li>
@@ -51,22 +51,22 @@
 
         <div class="row">
             <div class="col-md-8 mx-auto card mt-4 p-2" style="opacity:80%;">
-                <form method="post" action="home.html">
+                <form method="post" action="config/authentication.php">
                     <div class="form-group">
                       <label for="exampleInputEmail1">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                      <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
                       <label for="exampleInputPassword1">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
                     <div class="form-group form-check">
                       <input type="checkbox" class="form-check-input" id="exampleCheck1">
                       <label class="form-check-label" for="exampleCheck1">Remember Me</label>
                     </div>
-                    <button type="submit" class="btn btn-danger">Log in</button>
-                    or <a href="register.html" style="font-style:oblique;">Create an Account</a>
+                    <button type="submit" name="login" class="btn btn-danger">Log in</button>
+                    or <a href="register.php" style="font-style:oblique;">Create an Account</a>
                   </form>
             </div>
         </div>
