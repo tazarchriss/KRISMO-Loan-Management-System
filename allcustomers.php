cbeda27391a8368dfbf8f93156f7739508dd3ae0
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-05-16 02:40:14 +0300

    Update allcustomers.php

--- allcustomers.php
+++ allcustomers.php
@@ -14,32 +14,7 @@
     $qry1=mysqli_query($conn,$sql1);
 ?>
 
-
-  <!doctype html>
-  <html lang="en">
-    <head>
-      <!-- Required meta tags -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <!-- Font Awesome icons  -->
-
-        <!-- Font Awesome -->
-      <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-           <!-- digital fonts -->
-      <link href="http://fonts.cdnfonts.com/css/alarm-clock" rel="stylesheet">
-          <!-- DataTables -->
-  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">   
-      <title>www.krismo.com</title>
-    </head>
-    <style>
-      @import url('http://fonts.cdnfonts.com/css/alarm-clock');
-      </style>
-
+<?php include 'include/header.php'; ?>
           <!-- start of navbar-->
 
       <?php include 'include/navbar.php'; ?>
